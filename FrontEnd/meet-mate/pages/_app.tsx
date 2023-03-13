ed2e9831f770e8df61264b398a7eb3053cd0e16a
c0c0pang <coc1789@naver.com>
2023-03-14 03:43:11 +0900

    feat: 화면 구성에 대한 component 추가
    GlobalDiv는 Nav와 map에 화면 비율을 조정해줌

--- FrontEnd/meet-mate/pages/_app.tsx
+++ FrontEnd/meet-mate/pages/_app.tsx
@@ -1,14 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Nav from '@/desktopNav/Nav';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
-
+import { GlobalDiv } from '../styled-component/index-component/styled_index'
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
+
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <GlobalDiv>
+        <Nav />
+        <Component {...pageProps} />
+      </GlobalDiv>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
