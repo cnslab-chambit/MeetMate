acaabe6adaf3ca430eed2e89b1c04bef415ad9e3
c0c0pang <coc1789@naver.com>
2023-04-06 02:44:02 +0900

    feat: 장소 입력에 대한 결과를 리스트로 출력하는 기능 구현
    입력에 대한 키워드를 화면에 출력하며 예외처리는 아직 미구현

--- FrontEnd/meet-mate/desktop-search-list/SearchList.tsx
+++ FrontEnd/meet-mate/desktop-search-list/SearchList.tsx
@@ -1,4 +1,4 @@
-import { markerAtom } from '@/atom/atoms';
+import { clickState, inputState, markerAtom, roadState, searchState } from '@/atom/atoms';
 import { IMarkers } from '@/interface/desktop_intergace';
 import {
     AddressNameText,
@@ -15,16 +15,47 @@ import { useRecoilState } from 'recoil';
 
 function SearchList() {
     const [markerRecoil] = useRecoilState<IMarkers[]>(markerAtom);
+    const [road, setRoad] = useRecoilState(roadState)
+    const { start, end } = road
+    const [inputCheck, setInputCheck] = useRecoilState(inputState)
+    const [search, setSearch] = useRecoilState(searchState)
+    const [click, setClick] = useRecoilState(clickState)
+    const { start_point, end_point } = search
+    const onClick = (lng: string, lat: string, name: string) => {
+        if (inputCheck) {
+            setRoad({
+                start: name,
+                end: end
+            })
+            setSearch({
+                start_point: { lat, lng, img: '/images/start.svg' },
+                end_point: end_point
+            })
+            setClick(1)
+        }
+        else {
+            setRoad({
+                start: start,
+                end: name
+            })
+            setSearch({
+                start_point: start_point,
+                end_point: { lat, lng, img: '/images/end.svg' }
+            })
+            setClick(2)
+        }
+    }
+    console.log(click, inputCheck)
     return (
         <ListDiv>
             {markerRecoil?.map((data: IMarkers, index: number) =>
                 <ListInformationCard key={index}>
                     <NameTextDiv>
-                        <TitleNameText>{data.place_name}</TitleNameText>
-                        <GroupNameText>{data.category_group_name}</GroupNameText>
+                        <TitleNameText onClick={() => onClick(data.x, data.y, data.place_name)} >{data.place_name}</TitleNameText>
+                        {data.category_group_name && <GroupNameText>{data.category_group_name}</GroupNameText>}
                     </NameTextDiv>
-                    <RoadNameText>{data.road_address_name}</RoadNameText>
-                    <AddressNameText>지번: {data.address_name}</AddressNameText>
+                    {data.road_address_name && <RoadNameText>{data.road_address_name}</RoadNameText>}
+                    {data.address_name && <AddressNameText>지번: {data.address_name}</AddressNameText>}
                 </ListInformationCard>
             )}
         </ListDiv>
