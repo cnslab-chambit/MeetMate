6fc45ca59bc9d5711b909c69c9fe2e8daaeaa07e
c0c0pang <coc1789@naver.com>
2023-04-06 03:13:26 +0900

    feat: 미디어 쿼리를 이용하여 desktop 과 mobile 네비게이션 분리

--- FrontEnd/meet-mate/pages/_app.tsx
+++ FrontEnd/meet-mate/pages/_app.tsx
@@ -1,20 +1,34 @@
-import Nav from '@/desktopNav/Nav';
+import React from 'react';
+import DesktopMainPage from '@/desktopMain/DesktopMainPage';
+import NavButton from '@/desktopNav/NavButton';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { RecoilRoot } from 'recoil';
 import { GlobalDiv } from '../styled-component/index-component/styled_index'
+import { useMediaHook } from '@/custom-hook/MediaQueryHook';
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-
+  const [open, setOpen] = useState<boolean>(true)
+  const mediaCheck = useMediaHook()
   return (
-    <QueryClientProvider client={queryClient}>
-      <GlobalDiv>
-        <Nav />
-        <Component {...pageProps} />
-      </GlobalDiv>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        {mediaCheck ? (null) :
+          (
+            <>
+              <NavButton open={open} setOpen={setOpen} />
+              <GlobalDiv>
+                {open ? (<DesktopMainPage />) : (null)}
+                <Component {...pageProps} />
+              </GlobalDiv>
+            </>
+          )}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </RecoilRoot>
   )
 }
