c6bfafbd8d0aa67203a08d9ff4dc10601a66e7c5
c0c0pang <coc1789@naver.com>
2023-04-06 03:17:05 +0900

    refactor: 가독성을 높이기 위해 코드 정렬을 진행

--- FrontEnd/meet-mate/pages/_document.tsx
+++ FrontEnd/meet-mate/pages/_document.tsx
@@ -7,8 +7,10 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <Script src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_KEY}&libraries=services,clusterer&autoload=false`}
-          strategy="beforeInteractive"/>
+        <Script
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_KEY}&libraries=services,clusterer&autoload=false`}
+          strategy="beforeInteractive"
+        />
       </body>
     </Html>
   )
