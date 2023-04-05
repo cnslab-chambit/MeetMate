489074ca723956a100265679613cfbb217dfbea9
c0c0pang <coc1789@naver.com>
2023-04-06 03:14:02 +0900

    feat: 미디어 쿼리를 이용하여 desktop 과 mobile 메인 페이지 분리

--- FrontEnd/meet-mate/pages/index.tsx
+++ FrontEnd/meet-mate/pages/index.tsx
@@ -1,18 +1,12 @@
 import styles from '@/styles/Home.module.css'
-import { Map,MapMarker } from 'react-kakao-maps-sdk'
-
+import { Map, MapMarker } from 'react-kakao-maps-sdk'
+import { useMediaHook } from '@/custom-hook/MediaQueryHook'
+import Desktop from './desktop'
 export default function Home() {
+  const mediaCheck = useMediaHook()
   return (
     <>
-        <Map
-      center={{ lat: 33.5563, lng: 126.79581 }}
-      style={{ width: "100%", height: "100vh" ,maxHeight:"100vh"}}
-    >
-      <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-        {/* <div style={{ color: "#000" }}>Hello World!</div> */}
-      </MapMarker>
-    </Map>
-
+      {mediaCheck ? (null) : (<Desktop />)}
     </>
   )
 }
