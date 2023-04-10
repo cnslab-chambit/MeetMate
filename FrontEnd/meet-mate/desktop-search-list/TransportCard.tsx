275772b290731ad73ffe85c433a6c007895cf2e2
c0c0pang <coc1789@naver.com>
2023-04-10 16:46:11 +0900

    feat: 상세 대중교통 정보 페이지(미완)

---
+++ FrontEnd/meet-mate/desktop-search-list/TransportCard.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+
+function TransportCard() {
+    useEffect(() => {
+
+    }, [])
+    return (
+        <div>TransportCard</div>
+    )
+}
+
+export default TransportCard
