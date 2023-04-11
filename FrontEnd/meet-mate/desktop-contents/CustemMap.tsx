eb7cc9ce762c2bb3fbfefa800f10b2ef678cad94
c0c0pang <coc1789@naver.com>
2023-04-11 21:41:45 +0900

    feat: 장소 입력 및 길찾기에 대한 조건 렌더링 구현

--- FrontEnd/meet-mate/desktop-contents/CustemMap.tsx
+++ FrontEnd/meet-mate/desktop-contents/CustemMap.tsx
@@ -1,6 +1,7 @@
-import { clickState, inputState, markerState, searchState } from '@/atom/atoms';
+import { clickState, inputState, markerState, roadResultDataState, roadSearchResultState, searchState } from '@/atom/atoms';
+import { useDrawPolyLine, useSelectType } from '@/custom-hook/MapBaseHook';
 import { useEffect, useState } from 'react';
-import { Map, MapMarker, ZoomControl } from 'react-kakao-maps-sdk';
+import { Map, MapMarker, Polyline, ZoomControl } from 'react-kakao-maps-sdk';
 import { useRecoilState } from 'recoil';
 const ZoomCustem = () => {
   return (
@@ -13,6 +14,8 @@ function CustemMap() {
   const [marker, setMarker] = useRecoilState(markerState)
   const [click] = useRecoilState(clickState)
   const [search] = useRecoilState(searchState)
+  const [laneData, setLaneData] = useRecoilState(roadResultDataState);
+  const [roadSearchResult, setRoadSearchResult] = useRecoilState(roadSearchResultState)
   const { start_point, end_point } = search
   useEffect(() => {
     if (click == 1) {
@@ -22,35 +25,79 @@ function CustemMap() {
         img: start_point.img
       })
     }
-    else if (click == 2) {
+    if (click == 2) {
       setMarker({
         lat: +end_point.lat,
         lng: +end_point.lng,
         img: end_point.img
       })
     }
-
   }, [search])
-  console.log(marker, search)
+  console.log(marker, search, laneData)
   return (
-    <>
-      <Map
-        center={{ lat: marker.lat, lng: marker.lng }}
-        style={{ width: "100%", height: "100vh" }}
-      >
-        <ZoomCustem />
-        <MapMarker position={{ lat: marker.lat, lng: marker.lng }}
-          image={{
-            src: `${marker.img}`,
-            size: {
-              width: 55,
-              height: 55,
-            }, // 마커이미지의 크기입니다.
+    roadSearchResult ? (
+      <>
+        <Map
+          center={{ lat: ((+search.start_point.lat) + (+search.end_point.lat)) / 2, lng: ((+search.start_point.lng) + (+search.end_point.lng)) / 2 }}
+          style={{ width: "100%", height: "100vh" }}
+          level={8}
+        >
+          <ZoomCustem />
+          {roadSearchResult ? (
+            laneData.length !== 0 ?
+              laneData.map((data: any, index: number) =>
+                <Polyline key={index}
+                  path={useDrawPolyLine(data)}
+                  strokeColor={useSelectType(data.type).strokeColor}
+                  strokeWeight={5}
+                />
+              )
+              :
+              null
+          ) : (null)}
 
-          }} >
-        </MapMarker>
-      </Map>
-    </>
+          <MapMarker position={{ lat: +search.start_point.lat, lng: +search.start_point.lng }}
+            image={{
+              src: `${search.start_point.img}`,
+              size: {
+                width: 55,
+                height: 55,
+              },
+            }} >
+          </MapMarker>
+
+          <MapMarker position={{ lat: +search.end_point.lat, lng: +search.end_point.lng }}
+            image={{
+              src: `${search.end_point.img}`,
+              size: {
+                width: 55,
+                height: 55,
+              },
+            }} >
+          </MapMarker>
+
+        </Map>
+      </>
+    ) : (
+      <>
+        <Map
+          center={{ lat: marker.lat, lng: marker.lng }}
+          style={{ width: "100%", height: "100vh" }}
+        >
+          <ZoomCustem />
+
+          <MapMarker position={{ lat: marker.lat, lng: marker.lng }}
+            image={{
+              src: `${marker.img}`,
+              size: {
+                width: 55,
+                height: 55,
+              },
+            }} >
+          </MapMarker>
+        </Map>
+      </>
+    )
   )
 }
 
