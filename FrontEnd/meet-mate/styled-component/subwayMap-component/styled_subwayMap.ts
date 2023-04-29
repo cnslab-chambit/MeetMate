add49fa84bce9eb091686c2187e1e4a4236ed8a2
c0c0pang <coc1789@naver.com>
2023-04-30 03:17:47 +0900

    feat: 전철 페이지 화면 제작
    아직 미완성(검색 결과만 받아온 상태)

---
+++ FrontEnd/meet-mate/styled-component/subwayMap-component/styled_subwayMap.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const SubwayMapDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    background: rgba(175, 150, 187, 0.71);
+`
+export const SubwayPlainText = styled.h1`
+    font-size: 26px;
+`
