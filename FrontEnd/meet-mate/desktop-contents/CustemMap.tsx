7303f52b6c7fda8ed7ad5c8f2fd0132cd413a9a0
c0c0pang <coc1789@naver.com>
2023-04-30 03:14:14 +0900

    feat: 경로 그래프 점선 추가

--- FrontEnd/meet-mate/desktop-contents/CustemMap.tsx
+++ FrontEnd/meet-mate/desktop-contents/CustemMap.tsx
@@ -1,5 +1,5 @@
 import { clickState, inputState, markerState, roadResultDataState, roadSearchResultState, searchState } from '@/atom/atoms';
-import { useDrawPolyLine, useSelectType } from '@/custom-hook/MapBaseHook';
+import { useDrawPolyLine, useSelectType } from '@/custom-hook/mapBaseHook';
 import { useEffect, useState } from 'react';
 import { Map, MapMarker, Polyline, ZoomControl } from 'react-kakao-maps-sdk';
 import { useRecoilState } from 'recoil';
@@ -39,18 +39,28 @@ function CustemMap() {
       <>
         <Map
           center={{ lat: ((+search.start_point.lat) + (+search.end_point.lat)) / 2, lng: ((+search.start_point.lng) + (+search.end_point.lng)) / 2 }}
-          style={{ width: "100%", height: "100vh" }}
+          style={{ width: "100vw", height: "100vh" }}
           level={8}
         >
           <ZoomCustem />
           {roadSearchResult ? (
             laneData.length !== 0 ?
               laneData.map((data: any, index: number) =>
-                <Polyline key={index}
-                  path={useDrawPolyLine(data)}
-                  strokeColor={useSelectType(data.type).strokeColor}
-                  strokeWeight={5}
-                />
+                <>
+                  <Polyline key={index * 2}
+                    path={useDrawPolyLine(data)}
+                    strokeColor={useSelectType(data.class, data.type)}
+                    strokeWeight={7}
+                    strokeOpacity={0.8}
+                  />
+                  <Polyline key={index * 2 - 1}
+                    path={useDrawPolyLine(data)}
+                    strokeColor='#ffffff'
+                    strokeWeight={2}
+                    strokeStyle='dash'
+                    strokeOpacity={1}
+                  />
+                </>
               )
               :
               null
