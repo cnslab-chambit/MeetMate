b8a6be5d0d0e819bef79f6fa16bfde392d45154b
c0c0pang <coc1789@naver.com>
2023-05-01 17:50:00 +0900

    feat: style-component 추가

--- FrontEnd/meet-mate/m-styled-component/content-component/styled_find_bus.ts
+++ FrontEnd/meet-mate/m-styled-component/content-component/styled_find_bus.ts
@@ -3,12 +3,22 @@ import styled from "styled-components";
 export const TransferBox = styled.div`
     display: flex;
     flex-direction: column;
+    overflow-y: scroll;
+    max-height: 50vh;
 `;
+export const TransInfoDiv = styled.div`
+    /* position: absolute; */
+    width: 400px;
+    height: 80vh;
+    background-color: red;
+    left: 32%;
+    top: 0%;
+`
 
 export const TimeBox = styled.div`
     display:flex;
     flex-direction: column;
-    font-size: 2rem;
+    font-size: 15px;
     font-weight: 550;
     background-color: white;
     color:black;
@@ -18,15 +28,17 @@ export const TimeBox = styled.div`
 export const Boldpg = styled.div`
     font-family: 'GmarketSansBD';
     font-weight: 800;
-    font-size: 3rem;
+    font-size: 1.5rem;
 `;
 
 export const FlexBox = styled.div`
+    /* justify-content: center; */
     align-items: center;
     display: flex;
-    padding: 1rem;
     font-size: 1.5rem;
     font-weight: 400;
+    gap: 2px;
+    padding: 2px;
 `;
 
 export const FlexBoxCol = styled.div`
