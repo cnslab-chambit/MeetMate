5ed80394bbe455583d965576ccde6d1722b8980c
c0c0pang <coc1789@naver.com>
2023-05-08 21:34:58 +0900

    feat: 반응형으로 페이지 전환이 아닌 디바이스를 통해 페이지 전환을 위해 코드 수정

--- FrontEnd/meet-mate/pages/_app.tsx
+++ FrontEnd/meet-mate/pages/_app.tsx
@@ -9,24 +9,20 @@ import { ReactQueryDevtools } from 'react-query/devtools';
 import { RecoilRoot } from 'recoil';
 import { GlobalDiv } from '../styled-component/index-component/styled_index'
 import { useMediaHook } from '@/custom-hook/MediaQueryHook';
+import { TransInfoDiv } from '@/m-styled-component/content-component/styled_find_bus';
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   const [open, setOpen] = useState<boolean>(true)
-  const mediaCheck = useMediaHook()
+  // const mediaCheck = useMediaHook()
   return (
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
-        {mediaCheck ? (null) :
-          (
-            <>
-              <NavButton open={open} setOpen={setOpen} />
-              <GlobalDiv>
-                {open ? (<DesktopMainPage />) : (null)}
-                <Component {...pageProps} />
-              </GlobalDiv>
-            </>
-          )}
+        <NavButton open={open} setOpen={setOpen} />
+        <GlobalDiv>
+          {open ? (<DesktopMainPage />) : (null)}
+          <Component {...pageProps} />
+        </GlobalDiv>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </RecoilRoot>
