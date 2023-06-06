3f0fca9984c96270c2ee9a91f903c93da2e5a1f9
c0c0pang <coc1789@naver.com>
2023-06-07 02:37:42 +0900

    feat: 약속잡기에 필요한 기능 추가
    장소 입력에 따른 결과창과 장소찾기 버튼을 누르면 나오는 이벤트 화면 구현

--- FrontEnd/meet-mate/desktop-contents/PlacePage.tsx
+++ FrontEnd/meet-mate/desktop-contents/PlacePage.tsx
@@ -8,27 +8,73 @@ import {
   ContentInputIconDiv,
   ContentPlaceDiv,
   ContentInputButtonDiv,
+  ContentDiv,
 } from '@/styled-component/content-component/styled_content'
 import PlaceDialog from '@/dialog/PlaceDialog';
-import { useRecoilState } from 'recoil';
-import { countState, placeState, placeTextState } from '@/atom/atoms';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { coordinateDataState, countState, placeCoordinateState, placeIdState, placeMarkerState, placeState, roadPlaceState } from '@/atom/atoms';
 import CancelIcon from '../public/images/cancel.svg'
+import { placeCoordinateList } from '@/apis/apiStorage';
+import PublicTransportList from '@/desktop-search-list/PublicTransportList';
+import { IMarkers } from '@/interface/desktop_intergace';
+import PlaceSearchList from '@/desktop-search-list/PlaceSearchList';
+import SearchList from '@/desktop-search-list/SearchList';
+import CoordinateCategory from '@/desktop-search-list/CoordinateCategory';
 function PlacePage() {
 
   const [count, setCount] = useRecoilState(countState);
   const [open, setOpen] = useState<boolean>(false);
   const [placeAdd, setPlaceAdd] = useRecoilState(placeState);
-  const [text, setText] = useRecoilState(placeTextState)
+  const [placeCoordinate, setPlaceCoordinate] = useRecoilState(placeCoordinateState)
+  const setMarkerRecoil = useSetRecoilState<IMarkers[]>(placeMarkerState);
+  const [PlaceList, setPlaceList] = useRecoilState(roadPlaceState)
+  const [coordinateData, setCoordinateData] = useRecoilState(coordinateDataState)
+  const setPlaceId = useSetRecoilState(placeIdState)
   const onChange = (e: any) => {
     const { name, value } = e.target
-    setText({ ...text, [name]: value })
+    setPlaceCoordinate({ ...placeCoordinate, [name]: { name: value, lat: '', lng: '' } })
   }
   const onDelete = (target: number) => {
     const newPlaces = placeAdd.filter((element) => {
-      console.log(element.id, target)
       return element.id !== target
     })
     setPlaceAdd(newPlaces)
+    const newPlaceCoordinate = { ...placeCoordinate };
+    delete newPlaceCoordinate[target]
+    setPlaceCoordinate(newPlaceCoordinate)
+  }
+  const handleSubmit = (e: any, keyword: string, id: string) => {
+    e.preventDefault();
+    console.log(keyword)
+    const ps = new kakao.maps.services.Places();
+    if (ps === undefined || ps === null) return;
+    ps.keywordSearch(keyword, (data: IMarkers[], status: any, _pagination: any) => {
+      console.log(kakao.maps.services.Status.OK);
+      if (status === kakao.maps.services.Status.OK) {
+        let markers = [];
+
+        for (let i = 0; i < data.length; i++) {
+
+          markers.push({
+            address_name: data[i].address_name,
+            category_group_code: data[i].category_group_code,
+            category_group_name: data[i].category_group_name,
+            category_name: data[i].category_name,
+            distance: data[i].distance,
+            id: data[i].id,
+            phone: data[i].phone,
+            place_name: data[i].place_name,
+            place_url: data[i].place_url,
+            road_address_name: data[i].road_address_name,
+            x: data[i].x,
+            y: data[i].y
+          });
+        }
+        setMarkerRecoil(markers);
+      }
+    })
+    setPlaceList(false)
+    setPlaceId(id)
   }
   useEffect(() => {
     const newPlaces = [];
@@ -42,27 +88,43 @@ function PlacePage() {
     setPlaceAdd([...placeAdd, ...newPlaces]);
     setCount(0)
   }, [count])
+  const searchPlace = async () => {
+    let newLat = 0
+    let newLng = 0
+    let cnt = 0
+    for (let obj of Object.values(placeCoordinate)) {
+      newLat += (+obj?.lat)
+      newLng += (+obj?.lng)
+      cnt += 1
+    }
+    const data = await placeCoordinateList(newLng / cnt, newLat / cnt)
+    setCoordinateData(data)
+    setPlaceList(true)
+  }
   return (
     <ContentPlaceDiv>
-      <ContentInputForm>
+      <ContentDiv>
         {open ? (<PlaceDialog setOpen={setOpen} setCount={setCount}></PlaceDialog>) : (null)}
         <ContentInputDiv active={true}>
           {placeAdd.map((element, index: any) => {
             return index > 1 ? (
-              <ContentInputIconDiv>
-                <ContentInput name={element.id.toString()} value={text[element.id.toString()]} active={true} placeholder={element.current} key={element.id} onChange={onChange}></ContentInput>
+              <ContentInputIconDiv onSubmit={(e) => { handleSubmit(e, placeCoordinate[element.id.toString()].name, element.id.toString()); }}>
+                <ContentInput name={element.id.toString()} value={placeCoordinate[element.id.toString()]?.name} active={true} placeholder={element.current} key={element.id} onChange={onChange}></ContentInput>
                 <CancelIcon onClick={() => onDelete(element.id)} />
               </ContentInputIconDiv>
             ) : (
-              <ContentInput name={element.id.toString()} value={text[element.id.toString()]} active={false} placeholder={element.current} key={element.id} onChange={onChange}></ContentInput>
+              <ContentInputForm onSubmit={(e) => { handleSubmit(e, placeCoordinate[element.id.toString()].name, element.id.toString()); }}>
+                <ContentInput name={element.id.toString()} value={placeCoordinate[element.id.toString()]?.name} active={false} placeholder={element.current} key={element.id} onChange={onChange}></ContentInput>
+              </ContentInputForm>
             );
           })}
         </ContentInputDiv>
-      </ContentInputForm>
+      </ContentDiv>
       <ContentInputButtonDiv>
         <ContentInputButton type='button' value='장소 추가' onClick={() => setOpen(true)} _width='104px' _heigth='49px' />
-        <ContentInputButton type='button' value='장소 찾기' _width='104px' _heigth='49px' />
+        <ContentInputButton type='button' value='장소 찾기' onClick={() => searchPlace()} _width='104px' _heigth='49px' />
       </ContentInputButtonDiv>
+      {PlaceList ? (<CoordinateCategory />) : (<PlaceSearchList />)}
     </ContentPlaceDiv>
   )
 }
