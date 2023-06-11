3dfc50c1ca2f2bca19abb2d108e0de0415b65dac
c0c0pang <coc1789@naver.com>
2023-06-12 00:34:15 +0900

    feat: ReactQueryDevtools 기능 잠시 제거

--- FrontEnd/meet-mate/pages/_app.tsx
+++ FrontEnd/meet-mate/pages/_app.tsx
@@ -13,34 +13,35 @@ import { useRouter } from 'next/router';
 import { GlobalContainer } from '@/m-styled-component/index-component/styled_index';
 import SearchNav from '@/mobile-content/mobileNav/SearchNav';
 import MobileNav from '@/mobile-content/mobileNav/MobileNav';
-import { TransInfoDiv } from '@/m-styled-component/content-component/styled_find_bus';
-const queryClient = new QueryClient();
+import { LodingDiv } from '@/styled-component/content-component/styled_content';
+import LodingIcon from '../public/images/Loding.svg'
 export default function App({ Component, pageProps }: AppProps) {
   const [open, setOpen] = useState<boolean>(true)
   const mediaCheck = useMediaHook()
-  const router = useRouter(); 
+  const router = useRouter();
+  const queryClient = new QueryClient();
   return (
     <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
+      <QueryClientProvider client={queryClient} >
         {mediaCheck ? (
           <GlobalContainer>
-          {router.asPath==="/mobile/search" || router.asPath === "/mobile/road/search" || router.asPath === "/mobile/promise/search" ? <SearchNav/> : <MobileNav />}
-          <Component {...pageProps}/>
+            {router.asPath === "/mobile/search" || router.asPath === "/mobile/road/search" ? <SearchNav /> : <MobileNav />}
+            <Component {...pageProps} />
           </GlobalContainer>
         ) :
-        router.asPath==="/"
-        ?
-          (
-            <>
-              <NavButton open={open} setOpen={setOpen} />
-              <GlobalDiv>
-                {open ? (<DesktopMainPage />) : (null)}
-                <Component {...pageProps} />
-              </GlobalDiv>
-            </>
-          ):null}
-        <ReactQueryDevtools initialIsOpen={false} />
+          router.asPath === "/"
+            ?
+            (
+              <>
+                <NavButton open={open} setOpen={setOpen} />
+                <GlobalDiv>
+                  {open ? (<DesktopMainPage />) : (null)}
+                  <Component {...pageProps} />
+                </GlobalDiv>
+              </>
+            ) : null}
+        {/* <ReactQueryDevtools initialIsOpen={} /> */}
       </QueryClientProvider>
-    </RecoilRoot>
+    </RecoilRoot >
   )
 }
