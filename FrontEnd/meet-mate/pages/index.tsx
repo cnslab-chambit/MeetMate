79afeb9d55e793be4415332c872a3c8d7f3e87cb
c0c0pang <coc1789@naver.com>
2023-06-12 00:34:51 +0900

    feat: 현재 네비게이션에 따라 선택적 랜더링이 되도록 구현

--- FrontEnd/meet-mate/pages/index.tsx
+++ FrontEnd/meet-mate/pages/index.tsx
@@ -1,9 +1,20 @@
-import { Map,MapMarker } from 'react-kakao-maps-sdk'
+
+import { pageState } from "@/atom/atoms";
+import CustemMap from "@/desktop-contents/CustemMap";
+import PlaceMap from "@/desktop-contents/PlaceMap";
+import SubwayMap from "@/desktop-subway/SubwayMap";
+import { useRecoilValue } from "recoil";
 
 export default function Home() {
-  return (
-    <>
-      <Desktop />
-    </>
-  )
+  const navState = useRecoilValue(pageState)
+  console.log(navState)
+  if (navState.place) {
+    return (<PlaceMap />)
+  }
+  else {
+    return (
+      navState.subway ? (<SubwayMap></SubwayMap>) : (<CustemMap></CustemMap >)
+    )
+  }
+
 }
