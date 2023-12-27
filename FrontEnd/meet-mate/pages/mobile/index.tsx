2ddc357d15234eba65d90ccf70a3337851fa2e7f
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-12-27 16:56:56 +0900

    refactor: 분리 된 컴포넌트 적용

--- FrontEnd/meet-mate/pages/mobile/index.tsx
+++ FrontEnd/meet-mate/pages/mobile/index.tsx
@@ -1,57 +1,49 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import { CustomOverlayMap, Map,MapMarker } from 'react-kakao-maps-sdk'
-import { IMarkers, mapAtom } from '@/mobile-content/atom'
-import { useRecoilValue } from 'recoil'
-import { useEffect, useState } from 'react'
-import { BackButton, StoreInfoDiv, StoreName } from '@/m-styled-component/promise-component/promise_styled'
-import { IndexBackButton, IndexInfoDiv, IndexLocName } from '@/m-styled-component/index-component/styled_index'
-const inter = Inter({ subsets: ['latin'] })
+import { Inter } from "next/font/google";
+import { CustomOverlayMap } from "react-kakao-maps-sdk";
+import { IMarkers, mapAtom } from "@/mobile-content/atom";
+import { useRecoilValue } from "recoil";
+import { useEffect, useState } from "react";
+import KakaoMap from "@/components/Map/KakaoMap";
+import KakaoMarker from "@/components/Map/KakaoMarker";
+import LocationModal from "@/components/Map/LocationModal";
 
-export default function Home(props:any) {
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home(props: any) {
   const mapRecoil = useRecoilValue<IMarkers>(mapAtom);
   const [visible, setVisible] = useState(true);
   const Greet = "Your MeetMate!";
 
   useEffect(() => {
     setVisible(true);
-  },[]);
+  }, []);
 
   return (
     <div>
-      <Map
-      center={{ lat: parseFloat(mapRecoil.y), lng: parseFloat(mapRecoil.x) }}
-      style={{ width: "100%", height: "79vh" ,maxHeight:"100vh"}}
-      id="map"
-    >
-      <MapMarker position={{ lat: parseFloat(mapRecoil.y), lng: parseFloat(mapRecoil.x) }}
-      image={{
-        src: '/images/flag.svg',
-        size: {
-          width: 60,
-          height: 60,
-        },
-      }}
-      >
-      </MapMarker>
-      {visible ? 
-      <CustomOverlayMap
-      position={{lat: parseFloat(mapRecoil.y), lng: parseFloat(mapRecoil.x)}}
-      yAnchor={1.8}
-      zIndex={3}>
-        <IndexInfoDiv>
-              <IndexLocName>
-                {mapRecoil.place_name !== "장소 찾기" ? mapRecoil.place_name : Greet}
-              </IndexLocName>
-           <IndexBackButton onClick={() => setVisible(false)}>x</IndexBackButton>
-        </IndexInfoDiv>  
-      </CustomOverlayMap>
-    :
-    null}
-    </Map>
-
+      <KakaoMap x={mapRecoil.x} y={mapRecoil.y}>
+        <KakaoMarker x={mapRecoil.x} y={mapRecoil.y} src="/images/flag.svg" />
+        {visible ? (
+          <CustomOverlayMap
+            position={{
+              lat: parseFloat(mapRecoil.y),
+              lng: parseFloat(mapRecoil.x),
+            }}
+            yAnchor={1.8}
+            zIndex={3}
+          >
+            <LocationModal
+              placeName={
+                mapRecoil.place_name !== "장소 찾기"
+                  ? mapRecoil.place_name
+                  : Greet
+              }
+              onClose={() => setVisible(false)}
+            />
+          </CustomOverlayMap>
+        ) : null}
+      </KakaoMap>
     </div>
-  )
+  );
 }
+
+const S = {};
