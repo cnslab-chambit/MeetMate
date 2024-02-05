93581fa43e524cfb49d0bbf5ed1279420668bebf
c0c0pang <91197026+c0c0pang@users.noreply.github.com>
2024-02-06 02:45:26 +0900

    feat: Navigation과 Body를 Layout 컴포넌트로 통합

---
+++ components/Layout/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navigation } from './Navigation/Navigation'
+import { Body } from './body/Body'
+function Layout() {
+    return (
+        <>
+            <Navigation />
+            <Body />
+        </>
+    )
+}
+export default Layout
