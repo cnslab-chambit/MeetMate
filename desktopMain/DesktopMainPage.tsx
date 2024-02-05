48e80e9a77f5ba41be199933a34df90530a6dbf8
c0c0pang <91197026+c0c0pang@users.noreply.github.com>
2024-02-06 02:47:26 +0900

    feat: 통합된 Layout 으로 변경

--- desktopMain/DesktopMainPage.tsx
+++ desktopMain/DesktopMainPage.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import { ContentDiv } from '../styled-component/index-component/styled_index'
-import Contents from '@/desktop-contents/Contents';
-import Navigation from '@/components/Layout/Navigation/Navigation';
+import Layout from '@/components/Layout/index';
 function DesktopMainPage() {
   return (
     <ContentDiv>
-      <Navigation />
-      <Contents />
+      <Layout />
     </ContentDiv>
   )
 }
