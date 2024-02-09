8dfab48bd88dfc970f4c22f2063645d91760523e
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2024-02-09 14:31:54 +0900

    refactor: page 폴더 삭제 및 app 폴더 생성

--- components/NavigationButton/index.tsx
+++ components/NavigationButton/index.tsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import {
-  NavButtonDiv
-} from '@/components/NavigationButton/NavigationButton.styles'
-import ArrowIcon from '@/public/images/arrow.svg'
+import React from "react";
+import { NavButtonDiv } from "@/components/NavigationButton/NavigationButton.styles";
+import ArrowIcon from "@/public/images/arrow.svg";
 function NavigationButton({ open, setOpen }: any) {
   const onNavTogle = () => {
-    open ? setOpen(false) : setOpen(true)
-  }
+    open ? setOpen(false) : setOpen(true);
+  };
   return (
     <NavButtonDiv onClick={onNavTogle} active={open}>
-      <ArrowIcon className='arrow' />
+      <ArrowIcon className="arrow" />
     </NavButtonDiv>
-  )
+  );
 }
 
-export default NavigationButton
+export default NavigationButton;
