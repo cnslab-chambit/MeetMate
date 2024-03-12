3767048c9ea6089c31bd13a4aa93a0be41d1f86f
c0c0pang <91197026+c0c0pang@users.noreply.github.com>
2024-03-12 22:12:52 +0900

    feat: atom 에 있던 interface 분리 및 새로운 interface 추가

--- src/app/_atom/atom.ts
+++ src/app/_atom/atom.ts
@@ -1,54 +1,5 @@
 import { atom } from "recoil";
-
-export interface IMarkers {
-  address_name: string;
-  category_group_code: string;
-  category_group_name: string;
-  category_name: string;
-  distance: string;
-  id: string;
-  phone: string;
-  place_name: string;
-  place_url: string;
-  road_address_name: string;
-  x: string;
-  y: string;
-}
-
-export interface IEachLocation {
-  id: string;
-  location: string;
-  place_name: string;
-  x: string;
-  y: string;
-}
-
-export interface Iplace {
-  id: number;
-  current: string;
-}
-
-export interface IMobile {
-  place: boolean;
-  map: boolean;
-  bus: boolean;
-  subway: boolean;
-}
-
-export interface IStore {
-  category_name: string;
-  searchList: IStoreInfo[];
-}
-
-export interface IStoreInfo {
-  address: string;
-  id: number;
-  place_name: string;
-  place_url: string;
-  star_rate: number;
-  x: string;
-  y: string;
-}
+import { IEachLocation, IMarkers, IMobile, IStore, Iplace } from "../_interfaces/interface";
 
 export const markerAtom = atom<IMarkers[]>({
   key: "markerAtom2",
