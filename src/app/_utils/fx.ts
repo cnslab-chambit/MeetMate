5136d37d807a3c2e8e04b178f1c5722e8f815c99
c0c0pang <91197026+c0c0pang@users.noreply.github.com>
2024-03-14 23:22:40 +0900

    feat: 기존 컴포넌트에 있던 기능들을 유틸로 분리 및 삭제

--- src/app/_utils/fx.ts
+++ src/app/_utils/fx.ts
@@ -1,62 +1,8 @@
-import { IMarkers } from "./atom";
 import axios from "axios";
+import { IMarkers } from "../_interfaces/interface";
 const odsay = axios.create({
   baseURL: "https://api.odsay.com/v1/api/",
 });
-
-const setWayColor = (
-  trafficType: number,
-  buswayCode: number,
-  subwayCode: number
-) => {
-  if (trafficType === 3) {
-    return "#c8c7c7";
-  } else if (trafficType === 2) {
-    //bus -> type 11이면 마을버스?
-    if (buswayCode === 4 || buswayCode === 6 || buswayCode === 14) {
-      return "#E86359";
-    } else if (buswayCode === 1) {
-      return "#95C53C";
-    } else if (buswayCode === 3) {
-      return "#74a813";
-    } else if (buswayCode === 11) {
-      return "#2560e8";
-    } else if (buswayCode === 12) {
-      return "#74a813";
-    }
-  } else if (trafficType === 1) {
-    //지하철
-    if (subwayCode === 1) {
-      return "#11419F";
-    } else if (subwayCode === 2) {
-      return "#37B42D";
-    } else if (subwayCode === 3) {
-      return "#FA5F2C";
-    } else if (subwayCode === 4) {
-      return "#3E7AD6";
-    } else if (subwayCode === 5) {
-      return "#9A58C0";
-    } else if (subwayCode === 6) {
-      return "#9D5316";
-    } else if (subwayCode === 7) {
-      return "#97A05A";
-    } else if (subwayCode === 8) {
-      return "#F073A4";
-    } else if (subwayCode === 9) {
-      return "#C3A52D";
-    } else if (subwayCode === 9) {
-      return "#C3A52D";
-    } else if (subwayCode === 109) {
-      return "#A9022D";
-    } else if (subwayCode === 104) {
-      return "#7DC4A5";
-    } else if (subwayCode === 101) {
-      return "#70B7E5";
-    } else if (subwayCode === 116) {
-      return "#ffe600";
-    }
-  }
-};
 
 export const callApi = async (center: any) => {
   const response = await fetch(
@@ -80,9 +26,8 @@ export const findPlaceRoute = async (
     for (let i = 0; i < startPoint.length; i++) {
       let baseUrl = `https://api.odsay.com/v1/api/searchPubTransPathT?SX=${parseFloat(
         startPoint[i].x
-      )}&SY=${parseFloat(startPoint[i].y)}&EX=${store.x}&EY=${store.y}&apiKey=${
-        process.env.NEXT_PUBLIC_ODSAY_KEY
-      }`;
+      )}&SY=${parseFloat(startPoint[i].y)}&EX=${store.x}&EY=${store.y}&apiKey=${process.env.NEXT_PUBLIC_ODSAY_KEY
+        }`;
       const data = await fetch(baseUrl);
       const jsonData = await data.json();
       setPlaceRoute((prev: any) => [...prev, jsonData]);
