3a1aa6013e115a403a314cbdb1251bcbe51be5aa
c0c0pang <91197026+c0c0pang@users.noreply.github.com>
2024-03-22 00:23:29 +0900

    feat: onClick에 type 추가

--- src/app/(afterLogin)/@map/(.)promise/map/_component/CategoryButton.tsx
+++ src/app/(afterLogin)/@map/(.)promise/map/_component/CategoryButton.tsx
@@ -1,11 +1,15 @@
 import styles from "./categoryButton.module.css";
 import cx from "classnames";
 
-type Props = { isActive: boolean; children: React.ReactNode };
+type Props = {
+  isActive: boolean;
+  children: React.ReactNode;
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+};
 
-export default function CategoryButton({ isActive, children }: Props) {
+export default function CategoryButton({ isActive, children, onClick }: Props) {
   return (
-    <button className={cx(styles.button, isActive && styles.activedButton)}>
+    <button className={cx(styles.button, isActive && styles.activedButton)} onClick={onClick}>
       {children}
     </button>
   );
